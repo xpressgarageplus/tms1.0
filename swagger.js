a3a0e4f049f5526b1e2d63bb41979960bae56ea4
xpressgarageplus <xpressgarageplus@gmail.com>
2025-07-06 04:43:34 -0400

    ✅ Add placeholder swagger.js to fix Render deploy

---
+++ swagger.js
@@ -0,0 +1,10 @@
+// swagger.js
+const express = require('express');
+const router = express.Router();
+
+// Placeholder response
+router.get('/', (req, res) => {
+  res.send('Swagger docs placeholder');
+});
+
+module.exports = router;
